a82045273e914aa875433b606bd49a9540280591
Maxsem <max.semegen@gmail.com>
2020-05-10 12:31:45 -0400

    config file complete

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,28 @@
 const connection = require("./connection.js");
 
 const orm = {
-
-selectAll: async function (table) {
+  selectAll: async function(table) {
     const sql = "SELECT * FROM ??";
     const [rows] = await connection.query(sql, [table]);
 
     return rows;
+  },
+
+  insertOne: async function(table, col, value) {
+    const sql = "INSERT INTO ?? SET ?? = ?";
+
+    const [rows] = await connection.query(sql, [table, col, value]);
+
+    return rows;
+  },
+
+  updateOne: async function(table, col1, value, col2, id) {
+    const sql = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+
+    const [rows] = await connection.query(sql, [table, col1, value, col2, id]);
+
+    return rows;
+  }
 };
 
-insertOne{};
-
-updateOne{};
-}
-module.exports;
+module.exports = orm;
