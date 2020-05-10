6772e8458e7a5518ee96192bc307957b6dbadf6a
Maxsem <max.semegen@gmail.com>
2020-05-10 13:23:14 -0400

    controller file completed

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,7 +1,42 @@
 const express = require("express");
-
 const Burger = require("../models/burger");
 
 const router = express.Router();
+
+router.get("/", async function(req, res) {
+  const data = await Burger.select();
+  res.render("index", { burgers: data });
+});
+
+router.get("/api/burgers", async function(req, res) {
+  try {
+    const burgers = await Burger.select();
+    res.status(200).json({ data: burgers });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/api/burgers", async function(req, res) {
+  try {
+    const burger = new Burger(req.body);
+    burger.insert(burger.burger_name);
+    res.status(201).json(burger);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.patch("/api/burgers/:id", async function(req, res) {
+  let burger = await Burger.find(req.params.id);
+
+  if (!burger) return res.status(404).end();
+  try {
+    await Burger.update(req.params.id);
+    res.status(200).json(burger);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
