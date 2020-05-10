f51e2ca528d054abcf9ed94a87852643ac354a0a
Maxsem <max.semegen@gmail.com>
2020-05-10 11:49:23 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Insert initial seeds into the database
+INSERT INTO burgers (burger_name)
+VALUES ('Goat Cheese Burger');
+INSERT INTO burgers (burger_name)
+VALUES ('Spicy Jalapenos Cheeseburger');
+INSERT INTO burgers (burger_name)
+VALUES ('Heartattack Bacon Cheeseburger');
