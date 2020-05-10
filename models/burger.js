9916afbdcde47fd8988aee20000ee1258c134997
Maxsem <max.semegen@gmail.com>
2020-05-10 12:57:12 -0400

    class burger completed

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,46 @@
 const orm = require("../config/orm.js");
 
+class Burger {
+  constructor({ burger_name, devoured = false }) {
+    this.burger_name = burger_name;
+    this.devoured = devoured;
+  }
+
+  static async select() {
+    let burgers;
+    await orm
+      .selectAll("burgers")
+      .then(row => (burgers = row))
+      .catch(console.error);
+    return burgers;
+  }
+
+  static insert(burger) {
+      let newBurger; 
+      await orm 
+      .insertOne("burgers", "burger_name", burger)
+      .then(row => (newBurger = row))
+      .catch(console.error);
+      return newBurger;
+  }
+
+  static async update(id) {
+      let updateBurger;
+      await orm
+      .updateOne("burgers", "devoured", true, "id", id)
+      .then(row => (updateBurger = row))
+      .catch(console.error);
+      return updateBurger;
+  }
+
+  static async find(id) {
+      let result;
+      await orm
+      .findById("burgers", "id", id)
+      .then(row => (result = row))
+      .catch(console.error);
+      return result;
+  }
+}
+
 module.exports = Burger;
