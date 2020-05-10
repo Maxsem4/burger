fb1ba45e7b842ca6ac068bd22847e254438a8133
Maxsem <max.semegen@gmail.com>
2020-05-10 12:09:18 -0400

    add port for listener

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(require("./controllers/burgers_controllers"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
