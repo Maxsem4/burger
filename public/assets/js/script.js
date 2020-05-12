1f6ba869983d2f61968cf6fd2c2574c2cd03eda5
Maxsem <max.semegen@gmail.com>
2020-05-12 17:54:26 -0400

    changes to script and css

---
+++ public/assets/js/script.js
@@ -0,0 +1,32 @@
+document.getElementById("form-div").addEventListener("submit", event => {
+  event.preventDefault();
+
+  const addBurger = {
+    burger_name: document.getElementById("burger-input").value.trim()
+  };
+
+  fetch(`/api/burgers`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(addBurger)
+  }).then(response => {
+    if (response.ok) location.reload();
+  });
+});
+
+document.querySelectorAll(".eat-button").forEach(button => {
+  button.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const id = this.getAttribute("data-id");
+    const devoured = this.getAttribute("data-devoured");
+
+    fetch(`/api/burgers/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ devoured: devoured })
+    }).then(response => {
+      if (response.ok) location.reload();
+    });
+  });
+});
